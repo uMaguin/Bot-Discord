70e0f68cb1b19053a07acdc14c97c3fa224d4462
Rafael Henrique <contato@umaguin.com>
2020-05-19 02:48:22 -0300

    Update

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const prefix = "!";
-const commands = require("./scripts/commandsReader")(prefix);
+const config = require("./config.json");
+const commands = require("./scripts/commandsReader")(config.prefix);
 
 client.on("ready", () => {
   console.log(`Logado em ${client.user.tag}!`);
 });
 
 client.on("message", (msg) => {
-  if (!msg.author.bot) {
-    console.log(`${msg.author.username}: ${msg.content}`);
+  if (!msg.author.bot && msg.guild) {
+    if (config.debug) console.log(`${msg.author.username}: ${msg.content}`);
 
     const args = msg.content.split(" ");
     if (commands[args[0]]) commands[args[0]](client, msg);
   }
 });
-client.login("NzExMDIxNDgwOTEzMTQxODQx.Xr888Q.F0fiH6KJAzsQv2lo_s7-A79R2UY");
+client.login(config.token);
