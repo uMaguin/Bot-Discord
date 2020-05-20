f69db9a45df6997cc8be8aba3dab7325faef6e6f
Rafael Henrique <contato@umaguin.com>
2020-05-20 09:53:38 -0300

    Update

---
+++ commands/ajuda.js
@@ -0,0 +1,16 @@
+const config = require("../config.json");
+const commands = require("../scripts/commandsReader")(config.prefix);
+
+const descriptions = {
+  "!ajuda": "Use esses comandos para ver os comandos disponiveis",
+};
+
+module.exports = async (client, msg) => {
+  var texto = "Comandos:";
+  Object.keys(commands).forEach((command) => {
+    texto += `\n ${command}: ${
+      descriptions[command] ? descriptions[command] : "Não tem descricao"
+    }`;
+  });
+  msg.reply(texto);
+};
---
+++ scripts/unknowCommand.js
@@ -0,0 +1,7 @@
+module.exports = async (client, msg) => {
+  var message = msg.content.split(" ");
+  message = message[0];
+  msg.reply(
+    `Comando '${message}' não existe \n Digite '!ajuda' para obter a lista de comandos.`
+  );
+};
